e4b578e3ddea77b91c6cf13233fd10bac35505f5
Zhi Zeng <119023696+zengzhphy@users.noreply.github.com>
2023-07-31 13:42:00 +0800

    Update nav.js

--- en/nav.js
+++ en/nav.js
@@ -2,6 +2,6 @@ document.writeln("		<nav>");
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"index.html\">Home</a></li>");
 document.writeln("        		<li><a href=\"undergraduate.html\">Undergraduate</a></li>");
-document.writeln("        		<li><a href=\"links.html\">Links</a></li>");
+<!--document.writeln("        		<li><a href=\"links.html\">Links</a></li>");-->
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
