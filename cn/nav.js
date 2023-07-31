397f625170c21c81d8c8bc053d47f11274ddcafa
Zhi Zeng <119023696+zengzhphy@users.noreply.github.com>
2023-07-31 13:01:25 +0800

    Update nav.js

--- cn/nav.js
+++ cn/nav.js
@@ -1,8 +1,7 @@
 document.writeln("		<nav>");
 document.writeln("    		<ul>");
 document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
-document.writeln("        		<li><a href=\"doc.html\">文档</a></li>");
-document.writeln("        		<li><a href=\"starcharts.html\">星图</a></li>");
+document.writeln("        		<li><a href=\"undergraduate.html\">本科</a></li>");
 document.writeln("        		<li><a href=\"links.html\">链接</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
